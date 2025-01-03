1a01cd707ffaeaf45e08f08bad961e7fbb19c550
ElderDragonfly <ElderDragonfly@yandex.ru>
2025-01-03 17:26:50 +0300

    feat: add queue

--- src/queue.js
+++ src/queue.js
@@ -13,23 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor() {
+    this.head = null; // Голова очереди
+    this.tail = null; // Хвост очереди
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head; // Возвращаем связный список, начиная с головы
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value); // Создаем новый узел
+    if (this.tail) {
+      this.tail.next = newNode; // Присоединяем новый узел к хвосту
+    }
+    this.tail = newNode; // Обновляем хвост
+    if (!this.head) {
+      this.head = newNode; // Если очередь была пустой, обновляем голову
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return undefined; // Если очередь пуста
+    }
+    const value = this.head.value; // Сохраняем значение головы
+    this.head = this.head.next; // Перемещаем голову на следующий узел
+    if (!this.head) {
+      this.tail = null; // Если очередь стала пустой, обнуляем хвост
+    }
+    return value;
   }
 }
+
 
 module.exports = {
   Queue
